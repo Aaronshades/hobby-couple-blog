792dd441a522d6b84cd68f09b54b3b433a544776
Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
2020-07-29 13:07:27 +0200

    Adding fix to global css selector
    Issue: https://github.com/vercel/next.js/issues/10142

--- next.config.js
+++ next.config.js
@@ -1,6 +1,42 @@
+
+/**
+ * Stolen from https://stackoverflow.com/questions/10776600/testing-for-equality-of-regular-expressions
+ */
+const regexEqual = (x, y) => {
+  return (
+    x instanceof RegExp &&
+    y instanceof RegExp &&
+    x.source === y.source &&
+    x.global === y.global &&
+    x.ignoreCase === y.ignoreCase &&
+    x.multiline === y.multiline
+  );
+};
+
 module.exports = {
   target: 'serverless',
   webpack: function (config) {
+
+    const oneOf = config.module.rules.find(
+      (rule) => typeof rule.oneOf === 'object'
+    );
+
+    if (oneOf) {
+      const moduleCssRule = oneOf.oneOf.find(
+        (rule) => regexEqual(rule.test, /\.module\.css$/)
+        // regexEqual(rule.test, /\.module\.(scss|sass)$/)
+      );
+
+      if (moduleCssRule) {
+        const cssLoader = moduleCssRule.use.find(({ loader }) =>
+          loader.includes('css-loader')
+        );
+        if (cssLoader) {
+          cssLoader.options.modules.mode = 'local';
+        }
+      }
+    }
+
     config.module.rules.push({
       test: /\.md$/,
       use: 'raw-loader',
