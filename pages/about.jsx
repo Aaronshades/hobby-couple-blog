e54d67fcbfe7518d9bda6126d3444a53b924b444
Haakon Helmen Rusås <haakon.s.helmen@gmail.com>
2020-08-21 09:49:58 +0200

    WIP About page

--- pages/about.jsx
+++ pages/about.jsx
@@ -1,11 +1,18 @@
+import styles from './about/about.module.css';
+
 import Layout from '@components/Layout/Layout';
 
 const About = ({ title, description, ...props }) => {
   return (
     <>
       <Layout pageTitle={`${title} | About`} description={description}>
-        <h1 className="title">Welcome to our blog!</h1>
-        <p>This is going to be the about us page.</p>
+        <section className={styles.aboutPage}>
+          <div className={styles.imageContainer}>Image of something nice</div>
+          <div className={styles.textContainer}>
+            <h1 className="title">This is us!</h1>
+            <p>This is going to be the about us page.</p>
+          </div>
+        </section>
       </Layout>
     </>
   );
